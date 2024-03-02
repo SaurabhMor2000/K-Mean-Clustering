f294746d46a55ef2be16c6c0702dea9a63ed0c6b
SaurabhMor2000 <115579361+SaurabhMor2000@users.noreply.github.com>
2024-03-02 11:09:52 +0530

    Add files via upload

---
+++ Kmean.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.cluster import KMeans
+customer_data = pd.read_csv('C:\\Users\\morsa\\OneDrive\\Desktop\\Kmean project\\Mall_Customers.csv')
+#print(customer_data)
+customer_data.head()
+customer_data.shape
+customer_data.info
+customer_data.isnull().sum()
+X = customer_data.iloc[:,[3,4]].values
+print(X)
+# plt.figure(figsize=(7, 7))
+# plt.scatter(X[:, 1], X[:, 0], s=50)
+# plt.title('Raw Data Points')
+# plt.xlabel('Annual Income')
+# plt.ylabel('Spending Score')
+# plt.show()
+plt.figure(figsize=(8, 8))
+plt.scatter(customer_data['Spending Score (1-100)'], customer_data['Annual Income (k$)'], s=50, c='blue')
+plt.title('Customer Data')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.show()
+
+wcss = []
+
+for i in range(1,11):
+  kmeans = KMeans(n_clusters=i, init='k-means++', random_state=42)
+  kmeans.fit(X)
+
+  wcss.append(kmeans.inertia_)
+  sns.set()
+plt.plot(range(1,11), wcss)
+plt.title('The Elbow Point Graph')
+plt.xlabel('Number of Clusters')
+plt.ylabel('WCSS')
+plt.show()
+kmeans = KMeans(n_clusters=5, init='k-means++', random_state=0)
+
+# return a label for each data point based on their cluster
+Y = kmeans.fit_predict(X)
+
+print(Y)
+
+plt.figure(figsize=(8,8))
+plt.scatter(X[Y==0,0], X[Y==0,1], s=50, c='green', label='Cluster 1')
+plt.scatter(X[Y==1,0], X[Y==1,1], s=50, c='red', label='Cluster 2')
+plt.scatter(X[Y==2,0], X[Y==2,1], s=50, c='yellow', label='Cluster 3')
+plt.scatter(X[Y==3,0], X[Y==3,1], s=50, c='violet', label='Cluster 4')
+plt.scatter(X[Y==4,0], X[Y==4,1], s=50, c='blue', label='Cluster 5')
+
+# plot the centroids
+plt.scatter(kmeans.cluster_centers_[:,0], kmeans.cluster_centers_[:,1], s=100, c='cyan', label='Centroids')
+
+plt.title('Customer Groups')
+plt.xlabel('Annual Income')
+plt.ylabel('Spending Score')
+plt.show()
